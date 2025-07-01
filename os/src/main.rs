c226cfecdc96eba0efe683f4adb13102debdf032
Koschei <nitianzero@gmail.com>
2025-07-02 03:30:24 +0800

    feat: init

---
+++ os/src/main.rs
@@ -0,0 +1,34 @@
+#![no_std]
+#![no_main]
+
+mod lang_items;
+
+const STSCALL_EXIT: usize = 93;
+
+fn syscall(id: usize, args: [usize; 3]) -> isize {
+    let mut ret;
+    unsafe {
+        core::arch::asm!(
+        "ecall",
+        inlateout("x10") args[0] => ret,
+        in("x11") args[1],
+        in("x12") args[2],
+        in("x17") id
+        );
+    }
+    ret
+}
+
+pub fn sys_exit(xstate: i32) -> isize {
+    syscall(STSCALL_EXIT, [xstate as usize, 0, 0])
+}
+
+#[unsafe(no_mangle)]
+extern "C" fn _start() {
+    sys_exit(9);
+    // loop {}
+}
+
+// fn main() {
+//     // println!("Hello, world!");
+// }
