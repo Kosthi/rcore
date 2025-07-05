c1e3a0fce40d7ad33fadad0eaf644088891fbeb1
Koschei <nitianzero@gmail.com>
2025-07-06 01:50:13 +0800

    feat: implement print

--- os/src/main.rs
+++ os/src/main.rs
@@ -1,9 +1,13 @@
 #![no_std]
 #![no_main]
 
+use crate::console::print;
+
+mod console;
 mod lang_items;
 
-const STSCALL_EXIT: usize = 93;
+const SYSCALL_WRITE: usize = 64;
+const SYSCALL_EXIT: usize = 93;
 
 fn syscall(id: usize, args: [usize; 3]) -> isize {
     let mut ret;
@@ -19,12 +23,17 @@ fn syscall(id: usize, args: [usize; 3]) -> isize {
     ret
 }
 
+pub fn sys_write(fd: usize, buffer: &[u8]) -> isize {
+    syscall(SYSCALL_WRITE, [fd, buffer.as_ptr() as usize, buffer.len()])
+}
+
 pub fn sys_exit(xstate: i32) -> isize {
-    syscall(STSCALL_EXIT, [xstate as usize, 0, 0])
+    syscall(SYSCALL_EXIT, [xstate as usize, 0, 0])
 }
 
 #[unsafe(no_mangle)]
 extern "C" fn _start() {
+    println!("Hello from Rust!");
     sys_exit(9);
     // loop {}
 }
